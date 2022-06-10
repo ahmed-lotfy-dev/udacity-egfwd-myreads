06d92133a07f2fd13f05dbe0b2f93c420207d149
alotfy2019 <elshenawy19@gmail.com>
2022-06-10 14:39:19 +0200

    Splitting homepage components into shelf and book components

---
+++ src/components/Shelf.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Book from "./Book";
+export default class Shelf extends Component {
+  render() {
+    return (
+      <div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Currently Reading</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              <li>
+                <Book />
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
